c238661b3ab75db610ef132e1abd3ecad8e7093f
Percy Maldonado <maldonadoq.percy@gmail.com>
2023-05-27 21:02:50 -0300

    Adicionando nuevo comentario

--- JewelCollector/Program.cs
+++ JewelCollector/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 // Alteração realizada por Percy Maldonado
+// Outra alteração realizada por Percy Maldonado
