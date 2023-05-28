94b400789e4c58cf63e0e983f1e95b63902dfc89
Jesamin Zevallos <jesaminzev@gmail.com>
2023-05-27 21:03:33 -0300

    Adicao de outro comentario

--- JewelCollector/Program.cs
+++ JewelCollector/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 //Alteração realizada por Jesamin Zevallos
+//Outra alteração realizada por Jesamin Zevallos
