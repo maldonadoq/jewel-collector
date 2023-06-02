63632b726b05b5015bdba3b1e7f15576821a8399
Percy Maldonado <maldonadoq.percy@gmail.com>
2023-06-02 15:06:06 -0300

    multilevel and random cells

--- JewelCollector/Map.cs
+++ JewelCollector/Map.cs
@@ -34,4 +34,56 @@ public class Map
     {
         board[cell.x, cell.y] = cell;
     }
+
+    public void addRandomCells(int _numbers, string _type)
+    {
+        Random rnd = new Random();
+        int rows = board.GetLength(0);
+        int cols = board.GetLength(1);
+
+        for (int i = 0; i < _numbers; i++)
+        {
+            int x = rnd.Next(0, rows);
+            int y = rnd.Next(0, cols);
+
+            if (_type.Equals("JR"))
+            {
+                addCell(new Jewel(x, y, " JR ", 100, ConsoleColor.Red));
+            }
+            else if (_type.Equals("JG"))
+            {
+                addCell(new Jewel(x, y, " JG ", 50, ConsoleColor.Green));
+            }
+            else if (_type.Equals("JB"))
+            {
+                addCell(new Jewel(x, y, " JB ", 10, ConsoleColor.Blue));
+            }
+            else if (_type.Equals("##"))
+            {
+                addCell(new Obstacle(x, y, " ## ", ConsoleColor.Cyan));
+            }
+            else if (_type.Equals("$$"))
+            {
+                addCell(new Obstacle(5, 9, " $$ ", ConsoleColor.DarkGray));
+            }
+        }
+    }
+
+    public bool hasJewels()
+    {
+        bool isJewel = false;
+        for (int i = 0; i < board.GetLength(0); i++)
+        {
+            for (int j = 0; j < board.GetLength(1); j++)
+            {
+                if (board[i, j] is Jewel)
+                {
+                    isJewel = true;
+                    break;
+                }
+            }
+        }
+
+        return isJewel;
+    }
 }
--- JewelCollector/Program.cs
+++ JewelCollector/Program.cs
@@ -3,71 +3,83 @@
 
     public static void Main()
     {
-        // Create Map
-        Map map = new Map(10, 10);
-
-        // Create Robot
+        // Create robot
         Robot robot = new Robot(0, 0);
-        map.addCell(robot);
+        int max_level = 10;
 
-        // Add Obstacles
-        //Water
-        map.addCell(new Obstacle(5, 0, " ## ", ConsoleColor.Cyan));
-        map.addCell(new Obstacle(5, 1, " ## ", ConsoleColor.Cyan));
-        map.addCell(new Obstacle(5, 2, " ## ", ConsoleColor.Cyan));
-        map.addCell(new Obstacle(5, 3, " ## ", ConsoleColor.Cyan));
-        map.addCell(new Obstacle(5, 4, " ## ", ConsoleColor.Cyan));
-        map.addCell(new Obstacle(5, 5, " ## ", ConsoleColor.Cyan));
-        map.addCell(new Obstacle(5, 6, " ## ", ConsoleColor.Cyan));
-        //Tree
-        map.addCell(new Obstacle(5, 9, " $$ ", ConsoleColor.DarkGray));
-        map.addCell(new Obstacle(3, 9, " $$ ", ConsoleColor.DarkGray));
-        map.addCell(new Obstacle(8, 3, " $$ ", ConsoleColor.DarkGray));
-        map.addCell(new Obstacle(2, 5, " $$ ", ConsoleColor.DarkGray));
-        map.addCell(new Obstacle(1, 4, " $$ ", ConsoleColor.DarkGray));
-
-        // Add Jewels
-        map.addCell(new Jewel(1, 9, " JR ", 100, ConsoleColor.Red));
-        map.addCell(new Jewel(8, 8, " JR ", 100, ConsoleColor.Red));
-        map.addCell(new Jewel(9, 1, " JG ", 50, ConsoleColor.Green));
-        map.addCell(new Jewel(7, 6, " JG ", 50, ConsoleColor.Green));
-        map.addCell(new Jewel(9, 4, " JB ", 10, ConsoleColor.Blue));
-        map.addCell(new Jewel(2, 1, " JB ", 10, ConsoleColor.Blue));
-
-        bool running = true;
-        do
+        for (int level = 10; level <= max_level; level++)
         {
-            map.printMap();
-            robot.printScore();
+            // Create Map
+            Map map = new Map(level, level);
 
-            Console.Write("Enter the command: ");
-            ConsoleKeyInfo command = Console.ReadKey();
-            Console.WriteLine();
+            // Add Robot
+            robot.x = 0;
+            robot.y = 0;
+            map.addCell(robot);
 
-            if (command.KeyChar.Equals('q'))
+             // Add Obstacles
+            //Water
+            map.addCell(new Obstacle(5, 0, " ## ", ConsoleColor.Cyan));
+            map.addCell(new Obstacle(5, 1, " ## ", ConsoleColor.Cyan));
+            map.addCell(new Obstacle(5, 2, " ## ", ConsoleColor.Cyan));
+            map.addCell(new Obstacle(5, 3, " ## ", ConsoleColor.Cyan));
+            map.addCell(new Obstacle(5, 4, " ## ", ConsoleColor.Cyan));
+            map.addCell(new Obstacle(5, 5, " ## ", ConsoleColor.Cyan));
+            map.addCell(new Obstacle(5, 6, " ## ", ConsoleColor.Cyan));
+            //Tree
+            map.addCell(new Obstacle(5, 9, " $$ ", ConsoleColor.DarkGray));
+            map.addCell(new Obstacle(3, 9, " $$ ", ConsoleColor.DarkGray));
+            map.addCell(new Obstacle(8, 3, " $$ ", ConsoleColor.DarkGray));
+            map.addCell(new Obstacle(2, 5, " $$ ", ConsoleColor.DarkGray));
+            map.addCell(new Obstacle(1, 4, " $$ ", ConsoleColor.DarkGray));
+
+            // Add Jewels
+            map.addCell(new Jewel(1, 9, " JR ", 100, ConsoleColor.Red));
+            map.addCell(new Jewel(8, 8, " JR ", 100, ConsoleColor.Red));
+            map.addCell(new Jewel(9, 1, " JG ", 50, ConsoleColor.Green));
+            map.addCell(new Jewel(7, 6, " JG ", 50, ConsoleColor.Green));
+            map.addCell(new Jewel(9, 4, " JB ", 10, ConsoleColor.Blue));
+            map.addCell(new Jewel(2, 1, " JB ", 10, ConsoleColor.Blue));
+
+            /* map.addRandomCells(3, "JR");
+            map.addRandomCells(3, "JG");
+            map.addRandomCells(3, "JB");
+            map.addRandomCells(10, "##");
+            map.addRandomCells(5, "$$"); */
+
+            bool running = true;
+            do
             {
-                running = false;
-            }
-            else if (command.KeyChar.Equals('w'))
-            {
-                robot.move(map, robot.x - 1, robot.y);
-            }
-            else if (command.KeyChar.Equals('a'))
-            {
-                robot.move(map, robot.x, robot.y - 1);
-            }
-            else if (command.KeyChar.Equals('s'))
-            {
-                robot.move(map, robot.x + 1, robot.y);
-            }
-            else if (command.KeyChar.Equals('d'))
-            {
-                robot.move(map, robot.x, robot.y + 1);
-            }
-            else if (command.KeyChar.Equals('g'))
-            {
-                robot.collect(map);
-            }
-        } while (running);
+                map.printMap();
+                robot.printScore();
+
+                Console.Write("Enter the command: ");
+                ConsoleKeyInfo command = Console.ReadKey();
+                Console.WriteLine();
+
+                if (command.KeyChar.Equals('w'))
+                {
+                    robot.move(map, robot.x - 1, robot.y);
+                }
+                else if (command.KeyChar.Equals('a'))
+                {
+                    robot.move(map, robot.x, robot.y - 1);
+                }
+                else if (command.KeyChar.Equals('s'))
+                {
+                    robot.move(map, robot.x + 1, robot.y);
+                }
+                else if (command.KeyChar.Equals('d'))
+                {
+                    robot.move(map, robot.x, robot.y + 1);
+                }
+                else if (command.KeyChar.Equals('g'))
+                {
+                    robot.collect(map);
+                }
+
+                running = map.hasJewels();
+            } while (running);
+        }
     }
 }
